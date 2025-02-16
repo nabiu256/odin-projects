69246c20c9272273ccbebdfd414fd7fa7e0e27c5
Nabiu <45703020+nabiu256@users.noreply.github.com>
2025-02-17 00:48:59 +0100

    Library: JS code cleanup and documentation

--- library/script.js
+++ library/script.js
@@ -1,4 +1,8 @@
-// Setup
+//==========================
+//          State
+//==========================
+
+/** Holds the state of the application, in our case, an array of `Book` objects. */
 let bookLibrary = [
   new Book("The Hobbit", "J.R.R. Tolkien", 295, true),
   new Book("Gideon the Ninth", "Tamsyn Muir", 448, false),
@@ -7,6 +11,7 @@ let bookLibrary = [
 
 /**
  * Represents a book.
+ * 
  * @constructor
  * @param {string} title - The title of the book.
  * @param {string} author - The author of the book.
@@ -25,148 +30,20 @@ function Book(title, author, pages, isRead) {
   };
 }
 
+/** Toggles the state value `isRead`. */
 Book.prototype.toggleRead = function() {
   this.isRead = !this.isRead;
-  console.dir(this);
 }
 
-
 /**
- * Toggles the state value for the `isRead` property of a given book, by id.
+ * Creates and adds a book to the state array of the application.
  * 
- * @param {number} bookId Id of the book
- * @param {boolean} readValue Whether the book has been read or not
- */
-// const toggleRead = (bookId, readValue) => {
-//   bookLibrary.find((book) => book.id === bookId).isRead = readValue;
-//   console.dir(bookLibrary.find((book) => book.id === bookId));
-// }
-
-const bookGrid = document.getElementById("books-grid");
-const dialog = document.querySelector("dialog");
-const showBtn = document.querySelector("#dialog-show");
-const closeBtn = document.querySelector("#dialog-close");
-const booksForm = document.getElementById("books-form");
-
-/**
- * Grabs book information from the form and returns a Book object.
- * @returns {Book} Book object containing the form information.
- */
-const getBookFromInput = () => {
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  const pages = document.getElementById("pages").value;
-  const isRead = document.getElementById("read").checked;
-
-  return { title, author, pages, isRead };
-};
-
-/**
- * Creates an HTML card for the given book and attaches it to the books grid.
- *
- * @param {Book} book - The Book object we want to create the card from.
- * @param {HTMLElement} bookGrid - The HTML element holding the book cards.
- */
-const renderBookCard = (book) => {
-  const bookCard = document.createElement("div");
-  const title = document.createElement("p");
-  const author = document.createElement("p");
-  const pages = document.createElement("p");
-  const readBtn = document.createElement("input");
-  const readBtnLabel = document.createElement("label");
-  const removeBtn = document.createElement("button");
-
-  bookCard.classList.add("book-card");
-  bookCard.setAttribute("data-book-id", book.id);
-
-  readBtn.setAttribute("type", "checkbox");
-  readBtn.setAttribute("id", `read-${book.id}`);
-  readBtnLabel.setAttribute("for", "read");
-  if (book.isRead) readBtn.checked = true;
-  readBtn.addEventListener("input", () => book.toggleRead());
-
-  title.textContent = book.title;
-  author.textContent = book.author;
-  pages.textContent = `${book.pages} pages`;
-  readBtnLabel.textContent = "Read?";
-
-  removeBtn.addEventListener("click", () => removeBookFromLibrary(book.id));
-  removeBtn.innerHTML = "Delete"
-
-  bookCard.appendChild(title);
-  bookCard.appendChild(author);
-  bookCard.appendChild(pages);
-  bookCard.appendChild(readBtnLabel);
-  bookCard.appendChild(readBtn);
-  bookCard.appendChild(removeBtn);
-  bookGrid.appendChild(bookCard);
-};
-
-
-/**
- * Toggles the state value for the `isRead` property of a given book, by id.
- * 
- * @param {number} bookId Id of the book
- * @param {boolean} readValue Whether the book has been read or not
- */
-const toggleRead = (bookId, readValue) => {
-  bookLibrary.find((book) => book.id === bookId).isRead = readValue;
-}
-
-/**
- * @param {any} bookId Id of the book to remove
- *
- * Removes the HTML element of the Book, by using the book id.
- */
-const removeBookCard = (bookId) => {
-  let bookCard = document.querySelector(`[data-book-id="${bookId}"]`)
-  bookCard.remove();
-}
-
-/** 
- * Updates the current displayed Book cards according to the
- * state array `bookLibrary`.
-*/
-const updateUI = () => {
-  // Compare state array and current displayed books
-  // and see which ones need to be added or deleted
-
-  // 1. Get array of id's of currently displayed books
-  const displayedBooks = [];
-
-  bookGrid.querySelectorAll("[data-book-id]").forEach((bookElement) => {
-    displayedBooks.push(bookElement.dataset.bookId);
-  });
-
-  // 2. Compare with state array and see which books need to be added
-  // or removed from page
-  const removedElements = displayedBooks.filter(elt => !bookLibrary.find((book) => book.id === elt));
-  const addedElements = bookLibrary.filter(book => !displayedBooks.includes(book.id));
-
-  console.dir(displayedBooks);
-  console.dir(addedElements);
-
-  // 3. On the page, add elements to be added, remove elements to be removed
-  for (const bookId of removedElements) {
-    removeBookCard(bookId);
-  }
-
-  for (const book of addedElements) {
-    renderBookCard(book);
-  }
-}
-
-
-/**
  * @param {any} bookData Information on the book to add
  */
 const addBookToLibrary = (bookData) => {
-  // 1. Construct book with given data
-  const book = new Book(bookData.title, bookData.author, bookData.pages, bookData.read);
-  // 2. Append book to array (update state)
+  const book = new Book(bookData.title, bookData.author, bookData.pages, bookData.isRead);
   bookLibrary.push(book);
 };
-
 
 /**
  * @param {any} bookId Id of the book to be removed
@@ -178,6 +55,96 @@ const removeBookFromLibrary = (bookId) => {
   updateUI();
 }
 
+
+//==========================
+//            UI
+//==========================
+const bookGrid = document.getElementById("books-grid");
+const dialog = document.querySelector("dialog");
+const showBtn = document.querySelector("#dialog-show");
+const closeBtn = document.querySelector("#dialog-close");
+const booksForm = document.getElementById("books-form");
+
+/**
+ * Grabs book information from the form and returns it as an object.
+ * 
+ * @param {HTMLFormElement} form - The form element holding the values to use.
+ * @returns {any} Object containing the form information.
+ */
+const getBookFromInput = (form) => {
+  const title = form.querySelector("#title").value;
+  const author = form.querySelector("#author").value;
+  const pages = form.querySelector("#pages").value;
+  const isRead = form.querySelector("#read").checked;
+
+  return { title, author, pages, isRead };
+};
+
+/**
+ * Creates a Boo card for the given book and attaches it to the books grid.
+ *
+ * @param {Book} book - The Book object we want to create the card from.
+ */
+const renderBookCard = (book) => {
+  // We use innerHTML here to easily modify the HTML
+  const innerHTML = `
+<h3>${book.title}</h3>
+<div class="book-card__content">
+  <p class="book-card__author">${book.author}</p>
+  <p>${book.pages} pages</p>
+  <div>
+    <label for="read-${book.id}">Read?</label>
+    <input type="checkbox" id="read-${book.id}" ${book.isRead ? "checked" : ""}>
+  </div>
+</div>
+<button>Remove</button>
+`
+  const bookCard = document.createElement("div");
+  bookCard.innerHTML = innerHTML;
+  bookCard.classList.add("book-card");
+  bookCard.setAttribute("data-book-id", book.id);
+  bookGrid.appendChild(bookCard);
+
+  bookCard.querySelector("button").addEventListener("click", () => removeBookFromLibrary(book.id))
+  bookCard.querySelector('[type="checkbox"]').addEventListener("input", () => book.toggleRead());
+};
+
+/**
+ * Removes the HTML book card, by using the book id.
+ * 
+ * @param {any} bookId Id of the book to remove
+ */
+const removeBookCard = (bookId) => {
+  let bookCard = document.querySelector(`[data-book-id="${bookId}"]`)
+  bookCard.remove();
+}
+
+/** 
+ * Updates the current displayed Book cards according to the
+ * state array `bookLibrary`.
+*/
+const updateUI = () => {
+  // 1. Get array of id's of currently displayed books
+  const displayedBooks = [];
+
+  bookGrid.querySelectorAll("[data-book-id]").forEach((bookElement) => {
+    displayedBooks.push(bookElement.dataset.bookId);
+  });
+
+  // 2. Compare with state array and see which books need to be added
+  // or removed from page
+  const addElements = bookLibrary.filter(book => !displayedBooks.includes(book.id));
+  const removeElements = displayedBooks.filter(elt => !bookLibrary.find((book) => book.id === elt));
+
+  // 3. On the page, add elements to be added, remove elements to be removed
+  for (const bookId of removeElements) {
+    removeBookCard(bookId);
+  }
+
+  for (const book of addElements) {
+    renderBookCard(book);
+  }
+}
 
 showBtn.addEventListener("click", () => {
   booksForm.reset();
@@ -196,7 +163,7 @@ booksForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   // 1. Get book data from form
-  const bookData = getBookFromInput();
+  const bookData = getBookFromInput(e.target);
 
   // 2. Add the book to the state array
   addBookToLibrary(bookData);
