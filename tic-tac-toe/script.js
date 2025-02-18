1fb48646245cb38e87b9bc270e3c13c59117deea
Nabiu <45703020+nabiu256@users.noreply.github.com>
2025-02-18 05:38:22 +0100

    TicTacToe: finished rendering logic, cleanup code and styling

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,3 +1,7 @@
+/**
+ * Sets the state representation of the game. The main object is a 2d array called
+ * `board`, which can hold three possible values in each position (or cell): "X", "O", `null`.
+ */
 function GameBoard() {
   const rows = 3;
   const columns = 3;
@@ -6,14 +10,19 @@ function GameBoard() {
   const board = [];
 
   // Board initialization
-  for (let i = 0; i < rows; i++) {
-    board[i] = [];
-    for (let j = 0; j < columns; j++) {
-      board[i][j] = null;
+  const getBoard = () => board;
+
+  const initializeBoard = () => {
+    for (let i = 0; i < rows; i++) {
+      board[i] = [];
+      for (let j = 0; j < columns; j++) {
+        board[i][j] = null;
+      }
     }
   }
 
-  const getBoard = () => board;
+  initializeBoard();
+
   const resetBoard = () => {
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < columns; j++) {
@@ -31,11 +40,11 @@ function GameBoard() {
    * @param {number} column Column of the cell to be played.
    *
    * @returns {"X"|"O"|boolean|null} If a valid play, returns `true`, `false` for an invalid play (already
-   *                occupied cell. If end condition is met, returns either "X" or "O" for
+   *                occupied cell). If end condition is met, returns either "X" or "O" for
    *                win condition or `null` for stalemate.
    */
   const playCell = (token, row, column) => {
-    // Cell is not empty, cannot be played again.
+    // Either cell doesn't exist, or cell is not empty and cannot be played again.
     if ((row >= rows || column >= columns) || board[row][column] !== null) {
       return false;
     }
@@ -85,7 +94,6 @@ function GameBoard() {
 
   const printBoard = () => {
     const printableBoard = board.map((row) => row.map((cell) => cell == null ? " " : cell));
-
     console.log(printableBoard);
   }
 
@@ -93,8 +101,48 @@ function GameBoard() {
 }
 
 const displayController = (function() {
+  /**
+   * We grab the gameboard state and the actual rendered cells and compare
+   * them, updating any cells as necessary.
+   *
+   * @param {any} board 2d array holding the state of the game board.
+   */
   const renderBoard = (board) => {
+    const boardState = board.getBoard();
+    const renderedBoard = [];
 
+    const oSymbol = `
+<img class="o-symbol" src="./o-symbol.svg" alt="" />
+`
+    const xSymbol = `
+<img class="x-symbol" src="./x-symbol.svg" alt="" />
+`
+
+    for (let i = 0; i < 3; i++) {
+      renderedBoard[i] = [];
+      for (let j = 0; j < 3; j++) {
+        const nth = (i * 3) + (j + 1);
+        renderedBoard[i][j] = document.querySelector(`.game-board div:nth-child(${nth})`);
+      }
+    }
+
+    for (let i = 0; i < boardState.length; i++) {
+      for (let j = 0; j < boardState[0].length; j++) {
+        const renderedCell = renderedBoard[i][j];
+        const stateCell = boardState[i][j];
+
+        if (stateCell === "X") {
+          renderedCell.innerHTML = xSymbol;
+        } else if (stateCell === "O") {
+          renderedCell.innerHTML = oSymbol;
+        } else if (stateCell === null) {
+          renderedCell.innerHTML = "";
+        } else {
+          console.log("Invalid value in game state array.");
+          console.dir(boardState);
+        }
+      }
+    }
   }
 
   return { renderBoard };
@@ -106,6 +154,27 @@ const game = (function() {
   // There's player "X" and "O".
   let activePlayer = "X";
 
+  // Whenever a game is finished, it should not accept any more moves.
+  // We track this state with this variable.
+  let isFinished = false;
+
+  // Node that holds the win text
+  const winText = document.querySelector(".win-text");
+
+  const setupHandlers = () => {
+    // Set controls for the players. Basically, they can click on a cell to make a play.
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const nth = (i * 3) + (j + 1);
+        let cell = document.querySelector(`.game-board div:nth-child(${nth})`);
+        cell.addEventListener("click", () => playRound(i, j));
+      }
+    }
+
+    // Clicking the win text resets the game.
+    winText.addEventListener("click", () => resetHandler());
+  }
+
   const switchActivePlayer = () => {
     activePlayer = activePlayer === "X" ? "O" : "X";
   }
@@ -115,8 +184,17 @@ const game = (function() {
     console.log(`${activePlayer}'s player turn.`);
   }
 
+  const toggleFinished = () => isFinished = !isFinished;
+
   const playRound = (row, column) => {
+    // If game is finished, no moves are possible
+    if (isFinished) {
+      console.log("Game is paused, cannot accept any moves right now.")
+      return;
+    }
+
     const result = board.playCell(activePlayer, row, column);
+    displayController.renderBoard(board);
 
     if (result === false) {
       console.log("Invalid play, choose an available cell.");
@@ -124,31 +202,52 @@ const game = (function() {
       return;
     }
 
-    if (result === "X") {
-      console.log("Player 1 has won. Resetting board...");
-      board.resetBoard();
-      printRound();
-      return;
-    } else if (result === "O") {
-      console.log("Player 2 has won. Resetting board...");
-      board.resetBoard();
-      printRound();
-      return;
-    }
-
-    if (result === null) {
-      console.log("Stalemate, resetting board...");
-      board.resetBoard();
-      printRound();
-      return;
+    // If any of the following, game has met an end condition
+    switch (result) {
+      case "X":
+        console.log("Player 1 has won.");
+        winText.innerText = "Player 1 won! Click here to play again."
+        winText.classList.remove("hidden");
+        toggleFinished();
+        return;
+      case "O":
+        console.log("Player 2 has won.");
+        winText.innerText = "Player 2 won! Click here to play again."
+        winText.classList.remove("hidden");
+        toggleFinished();
+        return;
+      case null:
+        console.log("Stalemate.");
+        winText.innerText = "Stalemate. Click here to play again."
+        winText.classList.remove("hidden");
+        toggleFinished();
+        return;
     }
 
     switchActivePlayer();
     printRound();
   }
 
-  // Print first round
-  printRound();
+  const resetHandler = () => {
+    // We should only be able to reset the game whenever it has paused
+    if (isFinished) {
+      resetGame();
+      toggleFinished();
+    }
+  }
 
-  return { playRound }
+  const resetGame = async () => {
+    console.log("Resetting game...");
+    winText.innerHTML = "Waiting for round...";
+    winText.classList.add("hidden");
+    board.resetBoard();
+    displayController.renderBoard(board);
+    printRound();
+  }
+
+  setupHandlers();
+  printRound();
+  displayController.renderBoard(board);
+
+  return { playRound, resetHandler }
 })();
