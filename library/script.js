1f12f4b1c1d5de2550e5478e3bb623b956572493
Nabiu <45703020+nabiu256@users.noreply.github.com>
2025-02-19 05:48:35 +0100

    Library: refactored code into ES6 classes

--- library/script.js
+++ library/script.js
@@ -1,59 +1,72 @@
 //==========================
 //          State
 //==========================
+class Book {
+  /**
+  * Represents a book.
+  * 
+  * @constructor
+  * @param {string} title - The title of the book.
+  * @param {string} author - The author of the book.
+  * @param {string} pages - Total pages of the book.
+  * @param {read} isRead - Whether the book has been read or not.
+  */
+  constructor(title, author, pages, isRead) {
+    this.id = self.crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.info = function() {
+      return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead ? "read" : "not read yet"}.`;
+    };
+  }
+
+  /** Toggles the state value `isRead`. */
+  toggleRead() {
+    this.isRead = !this.isRead;
+  }
+}
+
+class Library {
+  /**
+   * @param {any} books Books to initialize the Library with.
+   */
+  constructor(books) {
+    this.books = [];
+
+    for (const book of books) {
+      this.books.push(book);
+    }
+  }
+
+  /**
+   * Creates and adds a book to the state array of the application.
+   * 
+   * @param {any} bookData Information on the book to add
+   */
+  addBookToLibrary(bookData) {
+    const book = new Book(bookData.title, bookData.author, bookData.pages, bookData.isRead);
+    this.books.push(book);
+  };
+
+  /**
+   * @param {any} bookId Id of the book to be removed
+   *
+   * Removes a book from the state array, by id.
+   */
+  removeBookFromLibrary(bookId) {
+    this.books = this.books.filter((book) => book.id !== bookId);
+    updateUI();
+  }
+}
 
 /** Holds the state of the application, in our case, an array of `Book` objects. */
-let bookLibrary = [
+const bookLibrary = new Library([
   new Book("The Hobbit", "J.R.R. Tolkien", 295, true),
   new Book("Gideon the Ninth", "Tamsyn Muir", 448, false),
   new Book("Foundation", "Isaac Asimov", 244, true),
-];
-
-/**
- * Represents a book.
- * 
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
- * @param {string} pages - Total pages of the book.
- * @param {read} isRead - Whether the book has been read or not.
- */
-function Book(title, author, pages, isRead) {
-  this.id = self.crypto.randomUUID();
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.isRead = isRead;
-  this.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead ? "read" : "not read yet"
-      }.`;
-  };
-}
-
-/** Toggles the state value `isRead`. */
-Book.prototype.toggleRead = function() {
-  this.isRead = !this.isRead;
-}
-
-/**
- * Creates and adds a book to the state array of the application.
- * 
- * @param {any} bookData Information on the book to add
- */
-const addBookToLibrary = (bookData) => {
-  const book = new Book(bookData.title, bookData.author, bookData.pages, bookData.isRead);
-  bookLibrary.push(book);
-};
-
-/**
- * @param {any} bookId Id of the book to be removed
- *
- * Removes a book from the state array, by id.
- */
-const removeBookFromLibrary = (bookId) => {
-  bookLibrary = bookLibrary.filter((book) => book.id !== bookId);
-  updateUI();
-}
+]);
 
 
 //==========================
@@ -105,7 +118,7 @@ const renderBookCard = (book) => {
   bookCard.setAttribute("data-book-id", book.id);
   bookGrid.appendChild(bookCard);
 
-  bookCard.querySelector("button").addEventListener("click", () => removeBookFromLibrary(book.id))
+  bookCard.querySelector("button").addEventListener("click", () => bookLibrary.removeBookFromLibrary(book.id))
   bookCard.querySelector('[type="checkbox"]').addEventListener("input", () => book.toggleRead());
 };
 
@@ -133,8 +146,8 @@ const updateUI = () => {
 
   // 2. Compare with state array and see which books need to be added
   // or removed from page
-  const addElements = bookLibrary.filter(book => !displayedBooks.includes(book.id));
-  const removeElements = displayedBooks.filter(elt => !bookLibrary.find((book) => book.id === elt));
+  const addElements = bookLibrary.books.filter(book => !displayedBooks.includes(book.id));
+  const removeElements = displayedBooks.filter(elt => !bookLibrary.books.find((book) => book.id === elt));
 
   // 3. On the page, add elements to be added, remove elements to be removed
   for (const bookId of removeElements) {
@@ -166,7 +179,7 @@ booksForm.addEventListener("submit", (e) => {
   const bookData = getBookFromInput(e.target);
 
   // 2. Add the book to the state array
-  addBookToLibrary(bookData);
+  bookLibrary.addBookToLibrary(bookData);
 
   // 3. Update UI
   updateUI();
